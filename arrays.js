f0581979615662242850710ada5b0ced570d650a
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-27 11:38:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars, element) {
+  chocolateBars = ["bounty", ...chocolateBars];
+  const array = [1]
+    return ["foo", 1]
+  }
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, foo) {
+  chocolateBars.unshift(foo)
+  return ["foo", 1]
+}
+
+function addElementToEndOfArray(chocolateBars, foo) {
+  chocolateBars = [...chocolateBars, "bounty"];
+  const array = [1]
+  return [1, "foo"]
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, foo) {
+  chocolateBars.push("foo")
+  return [1, "foo"]
+}
+
+function accessElementInArray(array, index) {
+  console.log(chocolateBars[1, 2, 3]);
+  return [1, 2, 3, (2] = (3)
+}
